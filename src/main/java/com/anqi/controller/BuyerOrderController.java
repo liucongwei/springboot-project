4454e047213fcf61de28dce4a825768d440e61ac
liucongwei <liucongwei@guazi.com>
2019-12-05 13:19:07 +0800

    初始化

--- src/main/java/com/anqi/controller/BuyerOrderController.java
+++ src/main/java/com/anqi/controller/BuyerOrderController.java
@@ -39,22 +39,22 @@ public class BuyerOrderController {
 
     //创建订单
     @PostMapping("/create")
-    public ResultVO<Map<String,String>> create(@Valid OrderForm orderForm ,
-                                               BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            log.error("【创建订单】参数不正确，orderForm={}",orderForm);
+    public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm,
+                                                BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error("【创建订单】参数不正确，orderForm={}", orderForm);
             throw new SellException(ResultEnum.PARAM_ERROR.getCode(),
                     bindingResult.getFieldError().getDefaultMessage());
         }
-        OrderDTO orderDTO= OrderForm2OrderDTOConverter.convert(orderForm);
-        if(CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
             log.error("【创建订单】购物车不能为空");
             throw new SellException(ResultEnum.CART_EMPTY);
         }
-        OrderDTO createResult=orderService.create(orderDTO);
+        OrderDTO createResult = orderService.create(orderDTO);
 
-        Map<String,String> map=new HashMap<>();
-        map.put("orderId",createResult.getOrderId());
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", createResult.getOrderId());
 
         return ResultVOUtil.success(map);
 
@@ -63,32 +63,33 @@ public class BuyerOrderController {
     //订单列表
     @GetMapping("/list")
     public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
-                                         @RequestParam(value = "page",defaultValue = "0") Integer page,
-                                         @RequestParam(value = "size", defaultValue = "10") Integer size){
-        if(StringUtils.isEmpty(openid)){
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openid)) {
             log.error("【查询订单列表】openid为空");
             throw new SellException(ResultEnum.PARAM_ERROR);
         }
-        PageRequest request=new PageRequest(page,size);
-        Page<OrderDTO> orderDTOPage=orderService.findList(openid,request);
+        PageRequest request = new PageRequest(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
 
         return ResultVOUtil.success(orderDTOPage.getContent());
 //        return ResultVOUtil.success();
     }
+
     //订单详情
     @GetMapping("/detail")
     public ResultVO<OrderDTO> detail(@RequestParam("openid") String openid,
-                                     @RequestParam("orderId") String orderId){
+                                     @RequestParam("orderId") String orderId) {
 
-        OrderDTO orderDTO=buyerService.findOrderOne(openid,orderId);
+        OrderDTO orderDTO = buyerService.findOrderOne(openid, orderId);
         return ResultVOUtil.success(orderDTO);
     }
 
     //取消订单
     @PostMapping("/cancel")
     public ResultVO cancel(@RequestParam("openid") String openid,
-                           @RequestParam("orderId") String orderId){
-        buyerService.cancelOrder(openid,orderId);
+                           @RequestParam("orderId") String orderId) {
+        buyerService.cancelOrder(openid, orderId);
         return ResultVOUtil.success();
     }
 
